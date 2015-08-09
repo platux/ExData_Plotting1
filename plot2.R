3c8344f1edb7d40322dfc66bdc9f6808110eab4b
Valerio Platania <platux@fino.local>
2015-08-10 01:29:46 +0200

    plot2() implemented

---
+++ plot2.R
@@ -0,0 +1,29 @@
+## plot2.R
+## for Course Project 1 of Exploratory Data Analysis Course
+
+## function plot2
+## Call the function to produce a 480x480 png file named "plot2.png"
+## as per assignment requirement
+
+plot2 <- function () {
+    
+    library(graphics)
+    
+    consdata <- read.csv ("household_power_consumption.txt", sep=";", 
+                          na.strings = "?", skip = 66636, nrows = 2880, header = TRUE)
+    
+    names (consdata) <- c("Date","Time","Global_active_power",
+                          "Global_reactive_power","Voltage","Global_intensity",
+                          "Sub_metering_1","Sub_metering_2","Sub_metering_3")
+    
+    fdates <- 
+        strptime (paste(consdata$Date,consdata$Time), 
+                  format= "%m/%d/%Y %H:%M:%S")
+    
+    plot (fdates, consdata$Global_active_power, 
+          ylab="Global Active Power (kilowatts)", xlab="", pch = "|")
+
+    dev.copy (png, file = "plot2.png", width = 480, height = 480)
+    dev.off()
+    
+}
