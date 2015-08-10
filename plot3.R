24e04216b61b0211f2f5412757ab83d7e50119ab
Valerio Platania <platux@fino.local>
2015-08-10 02:06:05 +0200

    line size corrected

--- plot3.R
+++ plot3.R
@@ -26,7 +26,8 @@ plot3 <- function () {
     lines (fdates, consdata$Sub_metering_3, col ="blue")
     legend ("topright", legend = c("Sub_metering_1","Sub_metering_2", 
                                    "Sub_metering_3"),
-                        col=c("black","red","blue"), lty = c(1, 1, 1))
+                        col=c("black","red","blue"), lty = c(1, 1, 1), 
+                        lwd = c (1,1,1))
 
     dev.copy (png, file = "plot3.png", width = 480, height = 480)
     dev.off()
